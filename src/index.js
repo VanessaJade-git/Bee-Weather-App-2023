d2a17d927a3c913a27890789259f3f6f96c1e44b
VanessaJade-git <VANESSAFETTUCCIA@YAHOO.COM>
2023-12-19 16:08:43 +0800

    Added search using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 function handlesearch(event) {
   event.preventDefault();
   let searchinput = document.querySelector(".search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchinput.value;
+
+  searchCity(searchinput.value);
 }
 
 let searchFormElement = document.querySelector("#city-entered");
 console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handlesearch);
+
+function searchCity(city) {
+  let api = "67c51o1bccat73f84f450a30f6ffcb49";
+  let apiKey = `https://api.shecodes.io/weather/v1/current?query=${city}&key=67c51o1bccat73f84f450a30f6ffcb49&units=metric`;
+
+  axios.get(apiKey).then(displayTemperature);
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+searchCity("Paris");
